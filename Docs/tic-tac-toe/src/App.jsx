37880841bf2c8ece573a0e643d29cbe6686a3ff6
YeabNigusse <nigusseyeabsira@gmail.com>
2023-11-20 23:52:28 +0300

    lifted the state one level up to parent

--- Docs/tic-tac-toe/src/App.jsx
+++ Docs/tic-tac-toe/src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from "react"
 
-export function Square(){
+export function Square({value, onSquareClick}){
 
-  const [val, setVal] = useState(null)
-  function handClick(){
-    setVal('X')
-  }
-  return(<button onClick={handClick} className="square">{val}</button>)
+  return(<button className="square" onClick={onSquareClick}>{value}</button>)
 }
 
 export default function App(){
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handClick(i){
+    const nextSquare = squares.slice()
+    nextSquare[i] = 'X'
+    setSquares(nextSquare)
+}
+
   return (
     <>
     <div className="board-row">
-      <Square/>
-      <Square/>
-      <Square/>
+      <Square value={squares[0]} onSquareClick = {() => handClick(0)}/>
+      <Square value={squares[1]} onSquareClick={() => handClick(1)}/>
+      <Square value={squares[2]} onSquareClick={() => handClick(2)}/>
     </div>
 
     <div className="board-row">
-       <Square/>
-       <Square/>
-       <Square/>
+       <Square value={squares[3]} onSquareClick={() => handClick(3)}/>
+       <Square value={squares[4]} onSquareClick={() => handClick(4)}/>
+       <Square value={squares[5]} onSquareClick={() => handClick(5)}/>
     </div>
 
     <div className="board-row">
-      <Square/>
-      <Square/>
-      <Square/>
+      <Square value={squares[6]} onSquareClick={() => handClick(6)}/>
+      <Square value={squares[7]} onSquareClick={() => handClick(7)}/>
+      <Square value={squares[8]} onSquareClick={() => handClick(8)}/>
     </div>
     
     </>
