fda93a2b27aa45922e77872ccdf693821e00cca8
YeabNigusse <nigusseyeabsira@gmail.com>
2023-12-26 15:58:06 +0300

    learned handling events by using onclick

--- react-course/src/component/Todo.js
+++ react-course/src/component/Todo.js
@@ -1,12 +1,18 @@
 import '../App.css'
 
 function Todo(props){
+
+    const handleDel = () =>{
+        console.log(props.text)
+        console.log('Clicked!')
+    }
+
     return(
         <div className='parent'>
         <div className='card'>
           <h2>{props.text}</h2>
           <div className='btn'>
-            <button className='del'>Delete</button>
+            <button className='del' onClick={handleDel}>Delete</button>
           </div>
         </div>
     
