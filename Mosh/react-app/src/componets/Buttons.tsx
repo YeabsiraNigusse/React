629bb294216b739c19f87b5283ee9caf30cf820f
YeabNigusse <nigusseyeabsira@gmail.com>
2023-12-26 15:19:46 +0300

    done with styling our component

--- Mosh/react-app/src/componets/Buttons.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Buttons = () => {
-  return <button className="btn btn-primary">Buttons</button>;
-};
-
-export default Buttons;
---
+++ react-course/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='parent'>
+
+    <h1>My Todos</h1>
+    <div className='card'>
+      <h2>TITLE</h2>
+      <div className='btn'>
+        <button className='del'>Delete</button>
+      </div>
+    </div>
+
+    </div>
+    
+  );
+}
+
+export default App;
